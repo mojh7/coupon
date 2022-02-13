abcf44b1206cb4374e692bd33521f904a01559e6
momojh94 <mjh79017901@gmail.com>
2022-02-13 18:33:49 +0900

    feat: 프로젝트 생성

---
+++ src/test/kotlin/com/mojh/cms/CouponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mojh.cms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
