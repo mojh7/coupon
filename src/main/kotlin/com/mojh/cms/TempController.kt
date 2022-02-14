fa190b38c0011ec190c81ca067331b6461dc3722
momojh94 <mjh79017901@gmail.com>
2022-02-14 19:25:36 +0900

    feat: api 응답 클래스 추가

---
+++ src/main/kotlin/com/mojh/cms/TempController.kt
@@ -0,0 +1,31 @@
+package com.mojh.cms
+
+import com.mojh.cms.web.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TempController {
+
+    @GetMapping("/s")
+    fun succeed(): ApiResponse<*> {
+        return ApiResponse.succeed()
+    }
+
+    @GetMapping("/s2")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun succeed2() = ApiResponse.succeed("sss")
+
+    @GetMapping("/f")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun failed(): ApiResponse<*> {
+        return ApiResponse.failed(HttpStatus.BAD_REQUEST, "error~")
+    }
+
+    @GetMapping("/f2")
+    fun failed2(): ApiResponse<*> {
+        return ApiResponse.failed(HttpStatus.NOT_FOUND)
+    }
+}
