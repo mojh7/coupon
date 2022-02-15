62ea1f852ec4c0dde76a17846188e4ba02b80144
momojh94 <mjh79017901@gmail.com>
2022-02-15 15:26:42 +0900

    refactor: CustomException 클래스 네이밍 변경

--- src/main/kotlin/com/mojh/cms/exception/CustomException.kt
+++
@@ -1,9 +0,0 @@
-package com.mojh.cms.exception
-
-open class CustomException(override val message: String?) : RuntimeException()
-
-class BadRequestException(message: String?) : CustomException(message)
-
-class NotFoundException(message: String?) : CustomException(message)
-
-class ConflictException(message: String?) : CustomException(message)
