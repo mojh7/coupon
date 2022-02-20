94fe94129d5efdacdb6d2de83ec568fb5df7b615
momojh94 <mjh79017901@gmail.com>
2022-02-20 17:46:38 +0900

    fix: 미처 패킷지명 안 바뀐 것 수정

--- src/main/kotlin/com/mojh/cms/TempController.kt
+++ src/main/kotlin/com/mojh/cms/TempController.kt
@@ -1,6 +1,6 @@
 package com.mojh.cms
 
-import com.mojh.cms.web.ApiResponse
+import com.mojh.cms.common.ApiResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseStatus
