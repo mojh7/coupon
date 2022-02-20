790abbe5f58012338b5142ca2b7ea80dfb83b083
momojh94 <mjh79017901@gmail.com>
2022-02-20 18:22:25 +0900

    feat: CouponService 추가 및 todo 작성

---
+++ src/main/kotlin/com/mojh/cms/coupon/service/CouponService.kt
@@ -0,0 +1,16 @@
+package com.mojh.cms.coupon.service
+
+import com.mojh.cms.coupon.repository.CouponRepository
+import org.springframework.stereotype.Service
+
+@Service
+class CouponService(
+    private val couponRepository: CouponRepository
+) {
+    /* TODO: 쿠폰 서비스
+    TODO : 사용가능한 쿠폰 조회
+    TODO : 사용 및 만료된 쿠폰 조회
+    TODO : 해당 쿠폰 조회 
+    TODO : 쿠폰 사용
+     */
+}
