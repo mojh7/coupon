8884161defa2752f699c40a6f8228618ddb6192a
momojh94 <mjh79017901@gmail.com>
2022-02-27 22:17:23 +0900

    feat: MemberRepository 추가

---
+++ src/main/kotlin/com/mojh/cms/member/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package com.mojh.cms.member.repository
+
+import com.mojh.cms.member.entity.Member
+import org.springframework.data.repository.CrudRepository
+
+interface MemberRepository : CrudRepository<Member, Long> {
+}
