f40c2485135ff64a7e94b0a4e308339e8aa7c5c1
momojh94 <mjh79017901@gmail.com>
2022-03-07 07:28:58 +0900

    refactor: Member entity setter 접근 권한 수정

--- src/main/kotlin/com/mojh/cms/member/entity/Member.kt
+++ src/main/kotlin/com/mojh/cms/member/entity/Member.kt
@@ -7,20 +7,28 @@ import javax.persistence.*
 
 @Entity
 class Member(
+    accountId: String,
+    password: String,
+    role: Role,
+): BaseEntity() {
     @Column(length = 16, nullable = false, columnDefinition = "char")
-    var accountId: String,
+    var accountId: String = accountId
+        protected set
 
     @Column(length = 64, nullable = false, columnDefinition = "char")
-    var password: String,
+    var password: String = password
+        protected set
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    var role: Role,
+    var role: Role = role
+        protected set
 
     @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     @JoinColumn(name = "member_id")
     var coupons: MutableSet<MemberCoupon> = TreeSet<MemberCoupon>()
-): BaseEntity() {
+        protected set
+
     enum class Role {
         CUSTOMER, SELLER
     }
