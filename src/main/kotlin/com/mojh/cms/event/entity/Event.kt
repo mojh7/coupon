2b34a4e38a9e590ed1469b8b7af72b906d09ce6b
momojh94 <mjh79017901@gmail.com>
2022-03-07 07:34:23 +0900

    refactor: Event entity setter 접근 권한 수정

--- src/main/kotlin/com/mojh/cms/event/entity/Event.kt
+++ src/main/kotlin/com/mojh/cms/event/entity/Event.kt
@@ -2,36 +2,52 @@ package com.mojh.cms.event.entity
 
 import com.mojh.cms.common.BaseEntity
 import com.mojh.cms.coupon.entity.Coupon
+import com.mojh.cms.coupon.entity.MemberCoupon
 import com.mojh.cms.member.entity.Member
 import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.*
 
 @Entity
 class Event(
+    seller: Member,
+    title: String,
+    description: String,
+    status: Status = Status.CREATED,
+    startAt: LocalDateTime,
+    endAt: LocalDateTime
+) : BaseEntity() {
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
-    val seller: Member,
+    var seller: Member = seller
+        protected set
 
     @Column(length = 64, nullable = false)
-    var title: String,
+    var title: String = title
+        protected set
 
     @Column(length = 255, nullable = false)
-    var description: String,
+    var description: String = description
+        protected set
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    var status: Status = Status.CREATED,
+    var status: Status = status
+        protected set
 
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
-    var coupons: MutableSet<Coupon>,
+    var coupons: MutableSet<Coupon> = TreeSet<Coupon>()
+        protected set
 
     @Column(nullable = false)
-    var startAt: LocalDateTime,
+    var startAt: LocalDateTime = startAt
+        protected set
 
     @Column(updatable = false)
-    var endAt: LocalDateTime
-) : BaseEntity() {
+    var endAt: LocalDateTime = endAt
+        protected set
+
     enum class Status {
         CREATED, ENABLED, DISABLED
     }
