bcbb64adbe48636034a2ac992a2494a14fabcd77
momojh94 <mjh79017901@gmail.com>
2022-03-07 05:47:05 +0900

    feat: Coupon Entity fk 필드 event 추가

--- src/main/kotlin/com/mojh/cms/coupon/entity/Coupon.kt
+++ src/main/kotlin/com/mojh/cms/coupon/entity/Coupon.kt
@@ -1,6 +1,7 @@
 package com.mojh.cms.coupon.entity
 
 import com.mojh.cms.common.BaseEntity
+import com.mojh.cms.event.entity.Event
 import com.mojh.cms.member.entity.Member
 import java.time.LocalDateTime
 import javax.persistence.*
@@ -10,6 +11,10 @@ class Coupon(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     val seller: Member,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = true)
+    val event: Event? = null,
 
     @Column(length = 64, nullable = false)
     var title: String,
