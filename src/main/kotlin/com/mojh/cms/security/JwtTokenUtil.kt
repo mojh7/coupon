787666d89207744297bc4601dd2e08deebd9ca14
momojh94 <mjh79017901@gmail.com>
2022-03-08 16:20:47 +0900

    feat: JwtTokenUtil 추가
     - 액세스, 리프레시 토큰 생성과 유효성 체크, 액세스 토큰에서 id 추출하는 jwt 유틸 클래스

---
+++ src/main/kotlin/com/mojh/cms/security/JwtTokenUtil.kt
@@ -0,0 +1,72 @@
+package com.mojh.cms.security
+
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+@Component
+class JwtTokenUtil {
+    @Value("\${jwt.secret-key}")
+    private val SECRET_KEY_RAW: String = ""
+
+    @Value("\${jwt.access-token-valid-time}")
+    private val ACCESS_TOKEN_VALID_TIME: Long = 0
+
+    @Value("\${jwt.refresh-token-valid-time}")
+    private val REFRESH_TOKEN_VALID_TIME: Long = 0
+
+    private val SECRET_KEY: SecretKey by lazy {
+        Keys.hmacShaKeyFor(SECRET_KEY_RAW.toByteArray())
+    }
+
+    fun createAccessToken(id: Long): String {
+        val now = Date()
+        return Jwts.builder()
+            .setHeaderParam("typ", "JWT")
+            .setSubject("access")
+            .claim("id", id)
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + ACCESS_TOKEN_VALID_TIME))
+            .signWith(SECRET_KEY, SignatureAlgorithm.HS512)
+            .compact()
+    }
+
+    fun createRefreshToken(): String {
+        val now = Date()
+        return Jwts.builder()
+            .setHeaderParam("typ", "JWT")
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + REFRESH_TOKEN_VALID_TIME))
+            .signWith(SECRET_KEY, SignatureAlgorithm.HS512)
+            .compact()
+    }
+
+    fun getIdFrom(accessToken: String): Long {
+        return Jwts.parserBuilder()
+            .requireSubject("access")
+            .setSigningKey(SECRET_KEY)
+            .build()
+            .parseClaimsJws(accessToken)
+            .body["id"] as Long
+    }
+
+    fun validateTokenStatus(token: String): TokenStatus {
+        return try {
+            Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+            TokenStatus.VALID
+        } catch (ex: Exception) {
+            when(ex) {
+                is ExpiredJwtException -> TokenStatus.EXPIRED
+                else -> TokenStatus.INVALID
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/mojh/cms/security/TokenStatus.kt
@@ -0,0 +1,5 @@
+package com.mojh.cms.security
+
+enum class TokenStatus {
+    VALID, INVALID, EXPIRED
+}
