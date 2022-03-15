84a5f14f0450d629ba4649351c17ab226c70387b
momojh94 <mjh79017901@gmail.com>
2022-03-16 03:29:33 +0900

    refactor: Throwable도 받을 수 있게 CustomException 수정

--- src/main/kotlin/com/mojh/cms/common/exception/CustomException.kt
+++ src/main/kotlin/com/mojh/cms/common/exception/CustomException.kt
@@ -1,5 +1,14 @@
 package com.mojh.cms.common.exception
 
-open class CustomException(val errorCode: ErrorCode) : RuntimeException() {
+open class CustomException : RuntimeException {
+    val errorCode: ErrorCode
 
+    constructor(errorCode: ErrorCode) : super(errorCode.message) {
+        this.errorCode = errorCode
+    }
+
+    constructor(errorCode: ErrorCode, cause: Throwable) : super(errorCode.message, cause) {
+        this.errorCode = errorCode
+    }
 }
+
