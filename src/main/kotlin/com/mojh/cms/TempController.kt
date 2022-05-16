e207250aa1bc7e0b08019cf3b56d9daa6e4859cc
momojh94 <mjh79017901@gmail.com>
2022-05-16 15:11:33 +0900

    chore: 무중단 배포 확인 테스트

--- src/main/kotlin/com/mojh/cms/TempController.kt
+++ src/main/kotlin/com/mojh/cms/TempController.kt
@@ -10,17 +10,4 @@ class TempController(
 
     @GetMapping("/hello-world")
     fun testApi() = ApiResponse.succeed("hello world~")
-
-    @GetMapping("/pause")
-    @Throws(InterruptedException::class)
-    fun pause(): String? {
-        val s = 15
-        println("$s 초 sleep 시작")
-        for (idx in 1..s) {
-            Thread.sleep(1000)
-            println("$idx 초")
-        }
-        println("sleep 끝")
-        return "pause~"
-    }
 }
