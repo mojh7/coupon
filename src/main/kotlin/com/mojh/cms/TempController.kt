e0209e322b15cad5019a48ed461e716c1f61c9bc
momojh94 <mjh79017901@gmail.com>
2022-05-16 13:45:56 +0900

    chore: test를 위한 임시 api 추가

--- src/main/kotlin/com/mojh/cms/TempController.kt
+++ src/main/kotlin/com/mojh/cms/TempController.kt
@@ -2,14 +2,25 @@ package com.mojh.cms
 
 import com.mojh.cms.common.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/hello-world")
 class TempController(
 ) {
 
-    @GetMapping
+    @GetMapping("/hello-world")
     fun testApi() = ApiResponse.succeed("hello world~")
+
+    @GetMapping("/pause")
+    @Throws(InterruptedException::class)
+    fun pause(): String? {
+        val s = 15
+        println("$s 초 sleep 시작")
+        for (idx in 1..s) {
+            Thread.sleep(1000)
+            println("$idx 초")
+        }
+        println("sleep 끝")
+        return "pause~"
+    }
 }
