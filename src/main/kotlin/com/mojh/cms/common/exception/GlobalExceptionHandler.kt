a37718e9f02197de4629cd229b9678709205b68c
momojh94 <mjh79017901@gmail.com>
2022-07-02 21:52:05 +0900

    refactor: GlobalRestControllerAdvice 클래스 명 변경

--- src/main/kotlin/com/mojh/cms/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/mojh/cms/common/exception/GlobalExceptionHandler.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.util.function.Consumer
 
 @RestControllerAdvice
-class GlobalRestControllerAdvice {
+class GlobalExceptionHandler {
 
     companion object {
         private val LOGGER = LogManager.getLogger()
