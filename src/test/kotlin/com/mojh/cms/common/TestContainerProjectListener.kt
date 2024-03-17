7f7dabf91815c5fedd3cd195bb844139294d656b
mojh7 <mjh79017901@gmail.com>
2024-03-18 02:28:17 +0900

    test: testcontainer 설정 수정

--- src/test/kotlin/com/mojh/cms/common/TestContainerProjectListener.kt
+++ src/test/kotlin/com/mojh/cms/common/TestContainerProjectListener.kt
@@ -12,6 +12,8 @@ object TestContainerProjectListener : ProjectListener {
     val dockerComposeContainer = DockerComposeContainer<Nothing>(File("src/test/resources/compose-test.yml")).apply {
         withExposedService("mysql", 3306)
         withExposedService("redis", 6379)
+        withOptions("--compatibility")
+        withLocalCompose(true)
         waitingFor("mysql", Wait.forListeningPort())
         waitingFor("redis", Wait.forListeningPort())
     }
